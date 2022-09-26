478b63f46148bb6ea706928be48a6c2d42da54f0
magalaReuben <magalareuben60@gmail.com>
2022-09-26 19:09:11 +0300

    MyPdf class takes pdf as widgets with strings

---
+++ lib/lib/sync.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+
+//void main() {
+//  runApp(const MyPdf());
+//}
+
+class MyPdf extends StatelessWidget {
+  final String? pdf;
+  const MyPdf({super.key, required this.pdf});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(pdf: ""),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  final String? pdf;
+  const HomePage({super.key, required this.pdf});
+
+  @override
+  State<HomePage> createState() => _HomePage();
+}
+
+class _HomePage extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    print(widget.pdf);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: SfPdfViewer.asset('pdfs/QUESTIONS0042.pdf'),
+        ));
+  }
+}
